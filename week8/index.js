2082b0b23fc82ffc0f75ffbae52d861325221bbe
Michael Bey <michael.bey@stu.bmcc.cuny.edu>
2019-04-02 00:36:07 +0000

    Automatically backed up by Learn

---
+++ week8/index.js
@@ -0,0 +1,14 @@
+//events have two parts
+1. find element you undefined
+
+const button = document.getElementsById('click-me')
+
+button.addEventListener('click' onClick)('click' =>{})
+
+function onClick () {
+  
+  
+}
+
+const myArray = [4,5,6,7,8,9]
+console.log(myArray[2])
